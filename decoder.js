96d506fa5e189732015d2c42f7fca1a4a6001a9d
MoHaX27 <35339383+MoHaX27@users.noreply.github.com>
2025-04-04 13:21:16 +0400

    Update decoder.js

--- decoder.js
+++ decoder.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Расшифровщик
 // @namespace    http://tampermonkey.net/
-// @version      3.4
+// @version      3.5
 // @description  Automatically decode Bnovo booking information
 // @author       МоНаХ
 // @match        https://online.bnovo.ru/booking/general/*
