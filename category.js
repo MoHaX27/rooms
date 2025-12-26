d8cbf3e063369882590fbdbe8a860912d2e158f3
MoHaX27 <35339383+MoHaX27@users.noreply.github.com>
2025-12-26 11:41:48 +0400

    Update category.js

--- category.js
+++ category.js
@@ -11,130 +11,130 @@ window.roomCategories = {
         },
 	////////////////////////// СТРЕЖЕНЬ /////////////////////////
         "184076863": { //  Стандарт 2х местный
-            basePrice: 1400, // Стоимость основного места
-            extraPrice: 800, // Стоимость дополнительного места
-            baseOverPrice: 2170, // Стоимость основного места ВЫСОКИЙ СЕЗОН
-            extraOverPrice: 1070, // Стоимость дополнительного места ВЫСОКИЙ СЕЗОН
+            basePrice: 2080, // Стоимость основного места
+            extraPrice: 1200, // Стоимость дополнительного места
+            baseOverPrice: 2900, // Стоимость основного места ВЫСОКИЙ СЕЗОН
+            extraOverPrice: 1490, // Стоимость дополнительного места ВЫСОКИЙ СЕЗОН
             maxBaseGuests: 2, // Максимальное количество основных мест
 	    maxExtraGuests: 0, // Максимальное количество доп мест
             printName: "Стандарт", // Категоря номера для расшифровки
 	    checkName: "Стандарт 2х местный" // Категоря номера для счета
         },
         "3628094238": { //  Стандарт (2+1) 2х местный
-            basePrice: 1400,
-            extraPrice: 800,
-            baseOverPrice: 2170,
-            extraOverPrice: 1070,
+            basePrice: 2080,
+            extraPrice: 1200,
+            baseOverPrice: 2900,
+            extraOverPrice: 1490,
             maxBaseGuests: 2,
 	    maxExtraGuests: 1,
             printName: "Стандарт",
 	    checkName: "Стандарт 2х местный"
         },
         "3682376564": { //  Стандарт 3х местный
-            basePrice: 1270,
-            extraPrice: 800,
-            baseOverPrice: 1970,
-            extraOverPrice: 1070,
+            basePrice: 1880,
+            extraPrice: 1200,
+            baseOverPrice: 2480,
+            extraOverPrice: 1490,
             maxBaseGuests: 3,
 	    maxExtraGuests: 0,
             printName: "Стандарт",
 	    checkName: "Стандарт 3х местный"
         },
         "1889936227": { //  Двухместное размещение в 3х мест
-            basePrice: 1650,
-            extraPrice: 800,
-            baseOverPrice: 2550,
-            extraOverPrice: 1070,
+            basePrice: 2460,
+            extraPrice: 1200,
+            baseOverPrice: 3050,
+            extraOverPrice: 1490,
             maxBaseGuests: 2,
 	    maxExtraGuests: 0,
             printName: "Стандарт в 3х",
 	    checkName: "Двухместное размещение в 3х мест"
         },
         "372022831": { //  Комфорт 2 местный 2х комнатный
-            basePrice: 2160,
-            extraPrice: 900,
-            baseOverPrice: 2810,
-            extraOverPrice: 1270,
+            basePrice: 3200,
+            extraPrice: 1340,
+            baseOverPrice: 3530,
+            extraOverPrice: 1610,
             maxBaseGuests: 2,
 	    maxExtraGuests: 0,
             printName: "Комфорт",
 	    checkName: "Комфорт 2х местный 2х комнатный"
         },
         "3121715328": { //  Комфорт (2+1)
-            basePrice: 2160,
-            extraPrice: 900,
-            baseOverPrice: 2810,
-            extraOverPrice: 1270,
+            basePrice: 3200,
+            extraPrice: 1340,
+            baseOverPrice: 3530,
+            extraOverPrice: 1610,
             maxBaseGuests: 2,
 	    maxExtraGuests: 1,
             printName: "Комфорт",
 	    checkName: "Комфорт 2х местный 2х комнатный"
         },
         "2844400129": { //  Комфорт (2+2)
-            basePrice: 2160,
-            extraPrice: 900,
-            baseOverPrice: 2810,
-            extraOverPrice: 1270,
+            basePrice: 3200,
+            extraPrice: 1340,
+            baseOverPrice: 3530,
+            extraOverPrice: 1610,
             maxBaseGuests: 2,
 	    maxExtraGuests: 2,
             printName: "Комфорт",
 	    checkName: "Комфорт 2х местный 2х комнатный"
         },
         "1334863395": { //  Дуплекс 2х местный 2х комнатный
-            basePrice: 1700,
-            extraPrice: 800,
-            baseOverPrice: 2470,
-            extraOverPrice: 1070,
+            basePrice: 2520,
+            extraPrice: 1200,
+            baseOverPrice: 3310,
+            extraOverPrice: 1490,
             maxBaseGuests: 2,
 	    maxExtraGuests: 0,
             printName: "Дуплекс",
 	    checkName: "Дуплекс 2х местный 2х комнатный"
         },
         "1204873745": { //  Дуплекс (2+1)
-            basePrice: 1700,
-            extraPrice: 800,
-            baseOverPrice: 2470,
-            extraOverPrice: 1070,
+            basePrice: 2520,
+            extraPrice: 1200,
+            baseOverPrice: 3310,
+            extraOverPrice: 1490,
             maxBaseGuests: 2,
 	    maxExtraGuests: 1,
             printName: "Дуплекс",
 	    checkName: "Дуплекс 2х местный 2х комнатный"
         },
         "4109240830": { //  Улучшенный Стандарт 2х местный
-            basePrice: 1620,
-            extraPrice: 900,
-            baseOverPrice: 2390,
-            extraOverPrice: 1270,
+            basePrice: 2400,
+            extraPrice: 1340,
+            baseOverPrice: 3110,
+            extraOverPrice: 1610,
             maxBaseGuests: 2,
 	    maxExtraGuests: 0,
             printName: "Улучш Стандарт",
 	    checkName: "Улучшенный Стандарт 2х местный"
         },
         "1463994523": { //  Улучшенный Стандарт (2+1) 2х местный
-            basePrice: 1620,
-            extraPrice: 900,
-            baseOverPrice: 2390,
-            extraOverPrice: 1270,
+            basePrice: 2400,
+            extraPrice: 1340,
+            baseOverPrice: 3110,
+            extraOverPrice: 1610,
             maxBaseGuests: 2,
 	    maxExtraGuests: 1,
             printName: "Улучш Стандарт",
 	    checkName: "Улучшенный Стандарт 2х местный"
         },
         "2570918383": { //  Семейный 4х местный 2х комнатный
-            basePrice: 1240,
-            extraPrice: 800,
-            baseOverPrice: 1840,
-            extraOverPrice: 1070,
+            basePrice: 1840,
+            extraPrice: 1200,
+            baseOverPrice: 2350,
+            extraOverPrice: 1490,
             maxBaseGuests: 4,
 	    maxExtraGuests: 0,
             printName: "Семейный",
 	    checkName: "Семейный 4х местный 2х комнатный"
         },
         "2729833985": { //  Семейный (4+1)
-            basePrice: 1240,
-            extraPrice: 800,
-            baseOverPrice: 1840,
-            extraOverPrice: 1070,
+            basePrice: 1840,
+            extraPrice: 1200,
+            baseOverPrice: 2350,
+            extraOverPrice: 1490,
             maxBaseGuests: 4,
 	    maxExtraGuests: 1,
             printName: "Семейный",
